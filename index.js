b74c3177def8808cb8bc57c3fc51b2c962216802
dramikei <raghav2011rv@gmail.com>
2020-12-15 00:23:21 +0530

    Add routes

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
         (err, results) => {
             if (err) {
                 console.log(err);
-                res.status(500).json({ type: 'error', message: err });
+                return res.status(500).json({ type: 'error', message: err });
             }
             console.log(JSON.stringify(results, null, 2)); // results contains rows returned by server
         }
@@ -43,6 +43,92 @@ app.get('/', (req, res) => {
     res.json({ type: 'success', message: 'ok' });
 });
 
+app.get('/flight/remaining/:id', (req, res) => {
+    const { id } = req.params;
+    connection.execute(`call seatsRemaining(${id}, @remaining)`, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ type: 'error', message: err });
+            }
+        }
+    );
+    connection.execute(`select @remaining`, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ type: 'error', message: err });
+        }
+            console.log(JSON.stringify(results, null, 2)); // results contains rows returned by server
+            res.json({ type: 'success', message: results[0]});
+        }
+    );
+});
+
+
+app.get('/admin/flight/', (_, res) => {
+    connection.execute(`call fetchFlightsAdmin()`, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ type: 'error', message: err });
+            }
+            res.json({ type: 'success', message: results[0]});
+        }
+    );
+});
+
+app.post('/user/login', (req, res) => {
+    const {email, password} = req.body;
+    connection.execute(`call UserLogin("${email}", "${password}",@result)`, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ type: 'error', message: err });
+        }
+    }
+);
+connection.execute(`select @result`, (err, results) => {
+    if (err) {
+        console.log(err);
+        return res.status(500).json({ type: 'error', message: err });
+    }
+        console.log(JSON.stringify(results, null, 2)); // results contains rows returned by server
+        res.json({ type: 'success', message: results[0]});
+    }
+);
+});
+
+app.get('/ticket/:id', (req, res) => {
+    const {id} = req.params;
+    connection.execute(`call TicketDetails(${id})`, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ type: 'error', message: err });
+        }
+        res.json({ type: 'success', message: results[0][0]});
+    }
+);
+});
+
+app.post('/flight/filter', (req,res) => {
+    const {departure, arrival, dTime} = req.body;
+    connection.execute(`call FetchFlight("${departure}", "${arrival}", "${dTime}")`, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ type: 'error', message: err });
+        }
+        res.json({ type: 'success', message: results[0][0]});
+    });
+});
+
+app.get('/employee/checkin/:id', (req,res) => {
+    const ticketId = req.params.id;
+    //TODO: Fix
+    connection.execute(`call check_in(${ticketId}, 1)`, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ type: 'error', message: err });
+        }
+        res.json({ type: 'success', message: results});
+    });
+})
 
 // Start Server
 app.listen(process.env.PORT, () => {
